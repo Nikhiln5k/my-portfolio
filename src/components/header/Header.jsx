3f9b683281622b1d77996bb55162effc6178f96d
Nikhiln5k <nikhilskumar823@gmail.com>
2024-11-17 14:29:49 +0530

    style change

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -14,16 +14,16 @@ function Header() {
   return (
     <>
         <header>
-            <Navbar className='p-3 w-100 d-flex justify-content-md-between justify-content-around' sticky='top'>
-                <div className='d-flex align-items-center'>
+            <Navbar className='p-3 w-100 d-flex justify-content-md-between justify-content-center' sticky='top'>
+                <div className='d-flex align-items-center gap-2'>
                     <a style={style} href={'mailto:nikhilsk289@gmail.com'}>
                         <i class="fa-solid fa-envelope d-md-none d-inline fs-1"></i>
                         <span className='d-none d-md-inline'>nikhilsk289@gmail.com</span>
                     </a>
                     <a href={resume} download={resume} target='_blank' className='bg-light text-dark' style={{textDecoration:"none", borderRadius:"20px", padding:"10px 15px 10px 15px"}}>CV</a>
                 </div>
-                <div className='d-flex align-items-center'>
-                    <Link style={style} to={'/about'} className='fw-md-normal fw-bold'>About me</Link>
+                <div className='d-flex align-items-center gap-2'>
+                    <Link style={style} to={'/about'} className='fw-md-normal fw-bold'><span>About <span className='d-none d-md-inline'>me</span></span></Link>
                     <span className="me-3 d-none d-md-inline">/</span>
                     <Link style={style} to={'https://www.linkedin.com/in/nikhil-s-kumar-4b6127297/'}>
                         <i class="fa-brands fa-linkedin-in d-md-none d-inline fs-1"></i>
